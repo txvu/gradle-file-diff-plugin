0dfb8165939f62b857e9a793faa944c5e472decd
Trinh Vu <txvu100@gmail.com>
2022-03-23 21:43:40 -0500

    Added task files

---
+++ src/main/groovy/dev/3amlab/plugins/filediff/FileDiffPlugin.groovy
@@ -0,0 +1,15 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FileDiffPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.extentions.create('fileDiff', FileDiffExtension)
+
+        project.tasks.create('fileDiff', FileDiffTask) {
+            file1 = project.fileDiff.file1
+            file2 = project.fileDiff.file2
+        }
+    }
+}
